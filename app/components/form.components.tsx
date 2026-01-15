0219d7f90186d627a31ba33b52aa411c8386ca78
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-01-15 20:28:58 -0300

    feat: add weather API to form component, and adapt the project to it

--- app/components/form.components.tsx
+++ app/components/form.components.tsx
@@ -1,23 +1,25 @@
 "use client";
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 
-export default function Form({ onDataAvailable }: { onDataAvailable: (data: string) => void }) {
+export default function Form({ onDataAvailable }: { onDataAvailable: (data: {city: string, temperature: string}) => void }) {
     const inputRef = useRef<HTMLInputElement>(null);
-    useEffect(() => {
+
+    const handleClick = async () => {
         if(inputRef.current) {
-            onDataAvailable(inputRef.current.value);
-        }
-    }, []);
-    const handleClick = () => {
-        if(inputRef.current) {
-            onDataAvailable(inputRef.current.value);
+            const city = inputRef.current.value;
+            const response = await fetch(`https://goweather.xyz/v2/weather/${city}`);
+            const data = await response.json();
+            onDataAvailable({
+                city,
+                temperature: data.temperature                
+            })
         }
     };
 
     return(
         <div className="flex flex-col w-100 items-center gap-10 text-white bg-zinc-800 px-10 py-20 rounded-3xl">
             <h1 className="font-bold text-3xl">Cadastro de cidades</h1>
-            <input type="text" ref={inputRef} defaultValue="Nada" name="Cidade" placeholder="Cidade" className="outline-none border-2 p-2 w-50 rounded-xl" />
+            <input type="text" ref={inputRef} name="Cidade" placeholder="Cidade" className="outline-none border-2 p-2 w-50 rounded-xl" />
             <button type="button" className="px-8 py-2 font-bold bg-blue-600 rounded-xl cursor-pointer hover:opacity-80 active:scale-97" onClick={handleClick}>Enviar</button>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 "use client";
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 import Form from "./components/form.components";
 
 export default function Home() {
-  const [formData, setFormData] = useState<string|null>(null);
-  const handleDataFromForm = (data: string) => {
+  const [formData, setFormData] = useState<{city: string, temperature: string}|null>(null);
+  const handleDataFromForm = (data: {city: string, temperature: string}) => {
     setFormData(data);
   };
   return (
     <>
       <Form onDataAvailable={handleDataFromForm} />
-      {console.log(formData)};
+      {console.log(formData)}
+      {formData ? (
+        <div className="mt-6 w-100 text-white bg-zinc-800 px-10 py-5 rounded-3xl">
+          <h2 className="text-2xl font-bold text-center m-5">{formData.city}</h2>
+          <p className="text-xl text-center m-5">Temperatura: {formData.temperature}</p>
+        </div>
+      ): null}
     </>
   );
 }

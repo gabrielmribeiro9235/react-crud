a2a127f0371a120ad850982739c0cae9b0e4993b
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-01-20 10:49:38 -0300

    refactor: disable spellCheck in form

--- app/components/form.components.tsx
+++ app/components/form.components.tsx
@@ -26,7 +26,7 @@ export default function Form({ onDataAvailable }: { onDataAvailable: (data: {cit
     return(
         <div className="flex flex-col w-100 items-center gap-10 text-white bg-zinc-800 px-10 py-20 rounded-3xl">
             <h1 className="font-bold text-3xl">Cadastro de cidades</h1>
-            <input type="text" ref={inputRef} name="Cidade" autoComplete="off" placeholder="Cidade" className="outline-none border-2 p-2 w-50 rounded-xl" />
+            <input type="text" ref={inputRef} name="Cidade" autoComplete="off" spellCheck="false" placeholder="Cidade" className="outline-none border-2 p-2 w-50 rounded-xl" />
             <button type="button" className="px-8 py-2 font-bold bg-blue-600 rounded-xl cursor-pointer hover:opacity-80 active:scale-97" onClick={() => {
                 if(inputRef.current && inputRef.current.value !== ""){
                     handleClick();

517ca742bdfe60751b3c7723af2595fca5c1e085
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-01-21 20:15:10 -0300

    fix: correct the suppress hydratation warning, it was in html tag instead of body tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning={true} lang="pt-BR">
-      <body className={`${montserrat.variable} antialiased`}>
+    <html lang="pt-BR">
+      <body suppressHydrationWarning={true} className={`${montserrat.variable} antialiased`}>
         {children}
       </body>
     </html>

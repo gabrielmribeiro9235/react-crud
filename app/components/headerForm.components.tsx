d6febfec423af28f74435c40c5b39b419dff6516
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-02-07 11:25:13 -0300

    refactor(headerForm): replace div with form to enable submit via Enter

--- app/components/headerForm.components.tsx
+++ app/components/headerForm.components.tsx
@@ -27,17 +27,18 @@ export default function HeaderForm({ onDataAvailable,  }: { onDataAvailable: (da
 
     return(
         <header className="flex w-[100vw] justify-between fixed top-0 text-white bg-zinc-800 px-10 py-7 rounded-b-xl">
-            <div className="flex gap-5">
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                if(inputRef.current && inputRef.current.value !== ""){
+                    handleClick();
+                    inputRef.current.value = "";
+                    inputRef.current.placeholder = "Buscando...";
+                }
+            }} className="flex gap-5">
                 <h1 className="font-bold text-3xl">Cadastro de cidades</h1>
                 <input type="text" ref={inputRef} name="Cidade" autoComplete="off" spellCheck="false" placeholder="Cidade" className="outline-none border-2 p-2 w-50 rounded-xl" />
-                <button type="button" className="px-8 py-2 font-bold bg-blue-600 rounded-xl cursor-pointer hover:opacity-80 active:scale-97" onClick={() => {
-                    if(inputRef.current && inputRef.current.value !== ""){
-                        handleClick();
-                        inputRef.current.value = "";
-                        inputRef.current.placeholder = "Buscando...";
-                    }
-                }}>Enviar</button>
-            </div>
+                <button className="px-8 py-2 font-bold bg-blue-600 rounded-xl cursor-pointer hover:opacity-80 active:scale-97" >Enviar</button>
+            </form>
         </header>
     );
 }
